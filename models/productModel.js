c0afab7ffe939cfb3bb7f7da9c5d55bd29b2dd23
Raz Hoshia <razhoshia@gmail.com>
2024-11-10 22:12:33 +0200

    bugfix upload image

--- models/productModel.js
+++ models/productModel.js
@@ -42,10 +42,37 @@ class Product extends AbstractModel {
   }
 
   async create(product) {
-    // Validate product attributes
+    const fs = require('fs');
+    const path = require('path');
+    const https = require('https');
+    const imageDirectory = path.join(__dirname, '..', 'assets/images/');
+    console.log(imageDirectory)
+  
     this.validateProduct(product);
+  
+    if (product.image) {
+      const url = product.image;
+      const fileName = product.name + '.jpg';
+      const filePath = path.join(imageDirectory, fileName);
+  
+      const file = fs.createWriteStream(filePath);
+      https.get(url, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+          file.close(); 
+          console.log('Image downloaded and saved as', filePath);
+        });
+      }).on('error', (err) => {
+        console.error('Error downloading image:', err.message);
+        fs.unlink(filePath, () => {});
+      });
+  
+      // Update product.image to local path
+      product.image = fileName;
+    }
     return super.create(product); // Use the inherited create method
   }
+  
 
   async update(filter, update) {
     // If the update includes product attributes, validate them
